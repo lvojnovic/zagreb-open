c3bf4355a9d1a7e90ae84c85b38b157070dc0d61
Lucija Vojnović <lucija.vojnovic@gmail.com>
2018-03-25 16:56:23 +0200

    cup of croatia has a subset od disciplines

--- resources/public/js/submitForm.js
+++ resources/public/js/submitForm.js
@@ -1,49 +1,73 @@
 $( document ).ready(function() {
     var rules = {
-      "Male" : {
-        category : ["Long Cycle", "Biathlon", "Jerk Duel"],
-        kettlebell : {
+      "5th Zagreb Open" : {
+        "Male" : {
+          category : ["Long Cycle", "Biathlon", "Jerk Duel"],
+          kettlebell : {
             "Long Cycle" : ["2x20 kg", "2x24 kg", "2x28 kg", "2x32 kg"],
             "Biathlon" : ["2x20 kg", "2x24 kg", "2x28 kg", "2x32 kg"],
             "Jerk Duel" : ["2x24 kg"]
+          },
+          bodyweight : ["-63 kg", "-68 kg", "-73 kg", "-78 kg",
+                        "-85 kg", "-95 kg", "+95 kg"]
         },
-        bodyweight : ["-63 kg", "-68 kg", "-73 kg", "-78 kg",
-                      "-85 kg", "-95 kg", "+95 kg"]
-      },
-      "Female" : {
-        category : ["Snatch", "Long Cycle", "Jerk", "Jerk Duel"],
-        kettlebell : {
+        "Female" : {
+          category : ["Snatch", "Long Cycle", "Jerk", "Jerk Duel"],
+          kettlebell : {
             "Snatch" : ["12 kg", "16 kg", "20 kg", "24 kg"],
             "Long Cycle" : ["2x12 kg", "2x16 kg", "2x20 kg"],
             "Jerk" : ["2x12 kg", "2x16 kg", "2x20 kg"],
             "Jerk Duel" : ["2x16 kg"]
+          },
+          bodyweight : ["-58 kg", "-63 kg", "-68 kg", "+68 kg"]
+        }
+      },
+      "Cup of Croatia" : {
+        "Male" : {
+          category : ["Long Cycle", "Biathlon"],
+          kettlebell : {
+            "Long Cycle" : ["2x24 kg", "2x32 kg"],
+            "Biathlon" : ["2x24 kg", "2x32 kg"]
+          },
+          bodyweight : ["-63 kg", "-68 kg", "-73 kg", "-78 kg",
+                        "-85 kg", "-95 kg", "+95 kg"]
         },
-        bodyweight : ["-58 kg", "-63 kg", "-68 kg", "+68 kg"]
+        "Female" : {
+          category : ["Snatch", "Long Cycle"],
+          kettlebell : {
+            "Snatch" : ["16 kg", "24 kg"],
+            "Long Cycle" : ["2x16 kg", "2x24 kg"],
+          },
+          bodyweight : ["-58 kg", "-63 kg", "-68 kg", "+68 kg"]
+        }
       }
     }
 
+    var competition = $("#competition");
     var sex = $("#sex");
     var category = $("#category");
     var kettlebell = $("#kettlebell");
     var bodyweight = $("#bodyweight");
 
     var onCategoryChange = function() {
+        var compValue = competition.val();
         var sexValue = sex.val();
         var categoryVal = category.val();
 
         kettlebell[0].options.length = 0; //reset
-        var newVals = rules[sexValue]['kettlebell'][categoryVal]
+        var newVals = rules[compValue][sexValue]['kettlebell'][categoryVal]
         newVals.forEach(function(c) {
             kettlebell[0].appendChild(new Option(c, c));
         });
     };
 
     var onChange = function() {
-        var sexValue = sex.val()
+        var compValue = competition.val();
+        var sexValue = sex.val();
 
         var repopulateOptions = function(cmp, name) {
             cmp[0].options.length = 0; //reset
-            var newVals = rules[sexValue][name]
+            var newVals = rules[compValue][sexValue][name]
             newVals.forEach(function(c) {
                 cmp[0].appendChild(new Option(c, c));
             });
@@ -54,6 +78,7 @@ $( document ).ready(function() {
         onCategoryChange();
     }
 
+    competition.on('change', onChange);
     sex.on('change', onChange);
     category.on('change', onCategoryChange);
 });
